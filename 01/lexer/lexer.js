cc25c15b9c0ee9dcf9074fa286ca83ac9290e613
liulinboyi <814921718@qq.com>
2022-07-17 00:24:56 +0800

    feat: 添加注释

--- 01/lexer/lexer.js
+++ 01/lexer/lexer.js
@@ -2,11 +2,14 @@ import token from '../token/token.js'
 class Lexer {
     constructor(input, position, readPosition, ch) {
         this.input = input
+        /* position则指向所输入字符串中与ch字节对应的字符 */
         this.position = position ? position : 0
+        /* readPosition始终指向所输入字符串中的“下一个”字符 */
         this.readPosition = readPosition ? readPosition : 0
         this.ch = ch ? ch : ''
         this.readChar(this)
     }
+    /* 读取input中的下一个字符，并前移其在input中的位置 */
     readChar(l /* Lexer实例 */ ) {
         if (l.readPosition >= len(l.input)) {
             l.ch = 0
@@ -95,6 +98,8 @@ class Lexer {
                 break
         }
 
+        // 在返回词法单元之前，位于所输入字符串中的指针会前移，
+        // 所以之后再次调用NextToken()时，l.ch字段就已经更新过了。
         l.readChar(this)
         return tok
     }
